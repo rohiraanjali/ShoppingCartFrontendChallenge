ef1d37dc1cb7302db39b7e4cd7cf8b54759770e6
Anjali Rohira <rohiraanjali@gmail.com>
2021-07-17 19:17:15 +0530

    added comments

--- Cart.js
+++ Cart.js
@@ -1,16 +1,14 @@
+// Fetching product data from API 
 
 const fetchData = async() => {
-  try {
+try {
     let { products } = await(await fetch("https://yellowriddle.netlify.app/products.json")).json();
-    cart = products.map((product) => ({...product, qty: 1}));
-    
+    cart = products.map((product) => ({...product, qty: 1}));    
     updateCart(cart)
     updatePrice(cart);
-  } catch (error) {
+} catch (error) {
     console.log(error);
-  }
-
-};
+}};
 
 fetchData();
 
@@ -22,7 +20,9 @@ function updateCart(cart) {
     container.innerHTML = "";
 
     for (let i = 0; i < cart.length; i++) {
-      let html = `
+    let html = `
+        <!-- Product cards  -->
+
         <div class="product-img-inner">
             <div class="product-img"> 
                 <img class="img" src="${cart[i].img}" />
@@ -31,7 +31,9 @@ function updateCart(cart) {
                 <small class="Product-seller">Seller:${cart[i].seller}
                     <img class="fAssured-true" src="https://static-assets-web.flixcart.com/www/linchpin/fk-cp-zion/img/fa_62673a.png" />
                 </small>
-          
+
+                <!-- Product details -->
+
                 <div class="Product-details">
                     <p class="final-price">${cart[i].finalPrice} ${cart[i].currency}</p>   &nbsp;
                     <p class="original-price">${cart[i].originalPrice} ${cart[i].currency}</p>&nbsp;
@@ -40,11 +42,17 @@ function updateCart(cart) {
                     <svg width="14" height="14" xmlns="http://www.w3.org/2000/svg" class="_3GN0Y0"><g fill="none"><path d="M-1-1h16v16H-1"></path><path d="M7 0C3.136 0 0 3.136 0 7s3.136 7 7 7 7-3.136 7-7-3.136-7-7-7zm.7 10.5H6.3V6.3h1.4v4.2zm0-5.6H6.3V3.5h1.4v1.4z" fill="#388e3c" class=""></path></g></svg>
                 </div>
                 </div>
+
+                <!-- Product delivery details -->
+
                 <div class="product-cart1-delivery-details">
                     <p class="product-delivery-date">Delivery by ${cart[i].delivery.estimatedDate} | Free <span class="strike-fee"> ₹${cart[i].delivery.originalDeliveryCharge}</span></p>
                     <small class="product-replacement">${cart[i].delivery.replacementPolicyDuration} Replacement Policy</small>
                 </div>
             </div>
+
+            <!-- Product cart action -> for updating quantiy, save for later and remove item -->
+
             <div class="cart-action">
             <div class="quantity">
                 <a href="#" onclick="decrementQty(${i})" id="quantity__minus"><span class="minus">-</span></a> &nbsp;
